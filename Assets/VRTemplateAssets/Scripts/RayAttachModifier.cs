ca00db8fe26842e1addea303f421eb16e4c2e57e
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-09-16 09:45:45 -0400

    Got it working again through hapter 8

--- Assets/VRTemplateAssets/Scripts/RayAttachModifier.cs
+++ Assets/VRTemplateAssets/Scripts/RayAttachModifier.cs
@@ -1,8 +1,8 @@
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
+// using UnityEngine.XR.Interaction.Toolkit.Interactables;
+// using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 namespace Unity.VRTemplate
 {
